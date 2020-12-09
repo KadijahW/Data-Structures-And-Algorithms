8ff24c0b9592ab85e8e4de776d8e2b8922f2e1e6
kadjiah <kadijahwilson@pursuit.org>
2020-12-08 20:45:14 -0500

    used recursion to create a preorder, inorder and postorder DFS

---
+++ Trees/printTreeAlphabeticalOrder.js
@@ -0,0 +1,76 @@
+// Q9.
+// Print the following tree in alphabetical order https://drive.google.com/uc?id=1WiTdeqPgpQlgCBgSuwRGCcjajAO3JHNw&export=download
+// Another text visualization of the tree could be
+//          (F)
+//     (D)      (J)
+//   (B) (E)  (H) (K)  
+// (A)(C)   (G)(I)
+// The tree is already built for you in the code below.
+
+class BinaryTreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// Create nodes
+let a = new BinaryTreeNode("A");
+let b = new BinaryTreeNode("B");
+let c = new BinaryTreeNode("C");
+let d = new BinaryTreeNode("D");
+let e = new BinaryTreeNode("E");
+let f = new BinaryTreeNode("F");
+let g = new BinaryTreeNode("G");
+let h = new BinaryTreeNode("H");
+let i = new BinaryTreeNode("I");
+let j = new BinaryTreeNode("J");
+let k = new BinaryTreeNode("K");
+
+// Connect nodes, thus creating the Tree
+f.left = d;
+f.right = j;
+
+d.left = b;
+d.right = e;
+
+b.left = a;
+b.right = c;
+
+j.left = h;
+j.right = k;
+
+h.left = g;
+h.right = i;
+
+// Implement your code here
+const printTreeAlphabeticalPreOrder = (root) => {
+  if(!root) return
+     // preorder traversal - prints in order
+     console.log(root.val)
+     printTreeAlphabeticalPreOrder(root.left)
+     printTreeAlphabeticalPreOrder(root.right)
+   }//FDBACEJHGIK
+    
+const printTreeAlphabeticalInOrder = (root) => {
+    if(!root) return
+       // inorder traversal - start from root check the left and then check the right
+
+       printTreeAlphabeticalInOrder(root.left)
+       console.log(root.val)
+       printTreeAlphabeticalInOrder(root.right)
+     }//
+
+const printTreeAlphabeticalPostOrder = (root) => {
+      if(!root) return
+         // postorder traversal - prints bottom up
+         printTreeAlphabeticalPostOrder(root.left)
+         printTreeAlphabeticalPostOrder(root.right)
+         console.log(root.val)
+}//ACBEDFGIHKJ  -  top root last thing to print
+
+console.log(printTreeAlphabeticalPreOrder(f))
+console.log(printTreeAlphabeticalInOrder(f))
+console.log(printTreeAlphabeticalPostOrder(f))
+
